c937871da566a5e0565ca911ba43fe2b7ee70f65
kjtm03 <terrallende03@gmail.com>
2017-11-16 11:09:10 -0500

    Add files via upload

---
+++ dbp/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="estilos.css">
+<link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+	<title>SuperChat</title>
+</head>
+<body>
+	<div id="contenedor">
+		<div id="caja-chat">
+			<div id="chat">
+				<div id="datos_chat">
+					<span style="color: red">Jogurmund: </span>
+					<span>Fale galera</span>
+					<span style="float: right;">09:04 pm</span>
+				</div>
+			</div>
+		</div>
+		<form method="POST" action="index.php">
+			<input type="text" name="nombre" placeholder="Ingrese su nombre">
+			<textarea name="mensaje" placeholder="Ingrese su mensaje"></textarea>
+			<input type="submit" name="enviar" value="Enviar">
+		</form>
+	</div>
+</body>
+</html>
